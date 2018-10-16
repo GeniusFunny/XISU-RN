3e52ec3201ee5bebbda17486ba512f3a3478cb92
GeniusFunny <geniusfunnyyh@gmail.com>
2018-10-16 23:57:24 +0800

    重构空教室查询

---
+++ src/constants/emptyClassroom.js
@@ -0,0 +1,60 @@
+import API_URLS from '../api/API_URLS'
+import {requestSuccess, requestFailed, requestBegin} from './request'
+import {FETCH_EMPTY_CLASSROOM, CHANGE_CLASSROOM_OPTIONS_DATE, CHANGE_CLASSROOM_OPTIONS_TIME} from '../actions'
+
+export function updateDate(date) {
+  return {
+    type: CHANGE_CLASSROOM_OPTIONS_DATE,
+    date: date
+  }
+}
+export function updateTime(time) {
+  return {
+    type: CHANGE_CLASSROOM_OPTIONS_TIME,
+    time: time
+  }
+}
+export function receiveEmptyClassroom(list) {
+  return {
+    type: FETCH_EMPTY_CLASSROOM,
+    list: list
+  }
+}
+function queryInfo(date, time) {
+  date = `${date.getFullYear()}-${date.getMonth() + 1 > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1)}-${date.getDate() > 9 ? date.getDate() : '0' + date.getDate()}`
+  let beginTime = time.split('-')[0]
+  let endTime = time.split('-')[1]
+  return {
+    cycleTime: {
+      dateBegin: date,
+      dateEnd: date,
+      cycleCount: 1,
+      cycleType: 1,
+      roomApplyTimeType: 0
+    },
+    timeBegin: beginTime,
+    timeEnd: endTime
+  }
+}
+export function fetchEmptyClassroom(date, time) {
+  return dispatch => {
+    dispatch(requestBegin())
+    let data = queryInfo(date, time)
+    fetch(API_URLS.emptyClassroom, {
+      method: 'POST',
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => {
+        dispatch(requestSuccess())
+        return JSON.parse(res._bodyInit)
+      })
+      .then(list => dispatch(receiveEmptyClassroom(list)))
+      .catch(err => {
+        dispatch(requestFailed(err.message))
+      })
+  }
+}
